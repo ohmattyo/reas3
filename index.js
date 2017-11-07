e3e34df5765c8ef5c193f2390a834bc3075b312d
Matt Osbourne <matt@osbourne.org>
2017-11-07 15:16:27 -0500

    now splits user stories apart and selects which user stories are important based on a given keyword

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const fs = require('fs');
+const _ = require('lodash');
 
-if(process.argv.length < 3){
-    // console.log('Usage: node ' + process.argv[1] + ' *filename*');
-    console.log('Usage: node index.js *filename*');
+if(process.argv.length < 4){
+    console.log('Usage: node index.js *filename* *keyword*');
     process.exit(1);
 }
 
-const readFile = function(fileName){
-    fs.readFile(fileName, 'utf8', function(err, data) {
-        if (err) throw err;
-        // console.log('OK: ' + fileName);
-        // console.log(data)
-        //return data.split('\n\n\n');
-        return data;
+const readFile = async function(fileName){
+    return await new Promise(function(resolve, reject){
+        fs.readFile(fileName, 'utf8', function(err, data) {
+            if(err){
+                reject(err);
+            }
+            resolve(data);
+        });
     });
 };
 
-const splitUserStories = function(uS){
-    return uS.split('\n\n\n');
+const splitUserStories = async function(uS){
+    return await new Promise(function(resolve){
+        resolve(uS.split('\n\n\n'));
+    });
+};
+
+const selectImportantUserStories = async function(userStoriesSplit, keyword){
+    return await new Promise(function(resolve){
+        let importantUserStories = [];
+        _.each(userStoriesSplit, function(userStory){
+            const isImportant = userStory.includes(keyword);
+            if(isImportant){
+                importantUserStories.push(userStory);
+            }
+        });
+        resolve(importantUserStories);
+    });
 };
 
 const execute = async function(){
     const userStoriesText = await readFile(process.argv[2]);
-    const userStories = await splitUserStories(userStoriesText);
-    console.log('test');
+    const userStoriesSplit = await splitUserStories(userStoriesText);
+    const importantUserStories = await selectImportantUserStories(userStoriesSplit, process.argv[3]);
 };
 
 execute();
-
-// console.log('test');
