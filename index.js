d832d8b08c737d2c8f6dc590f2494bbf97de178f
Matt Osbourne <matt@osbourne.org>
2017-11-07 14:14:45 -0500

    check in to sync

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ if(process.argv.length < 3){
     process.exit(1);
 }
 
-const fileName = process.argv[2];
-let userStoriesText = null;
+const readFile = function(fileName){
+    fs.readFile(fileName, 'utf8', function(err, data) {
+        if (err) throw err;
+        // console.log('OK: ' + fileName);
+        // console.log(data)
+        //return data.split('\n\n\n');
+        return data;
+    });
+};
 
-fs.readFile(fileName, 'utf8', function(err, data) {
-    if (err) throw err;
-    // console.log('OK: ' + fileName);
-    // console.log(data)
-    userStoriesText = data;
-});
-
-let userStories = userStoriesText.split('\n\n\n');
-
-console.log(userStories[1]);
+const splitUserStories = function(uS){
+    return uS.split('\n\n\n');
+};
 
 const execute = async function(){
-
+    const userStoriesText = await readFile(process.argv[2]);
+    const userStories = await splitUserStories(userStoriesText);
+    console.log('test');
 };
+
+execute();
 
 // console.log('test');
