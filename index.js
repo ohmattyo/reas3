b17837a68717beb2fc32a1a4874b40dcfc11c1a6
Matt Osbourne <matt@osbourne.org>
2017-11-13 09:07:29 -0500

    formatting changes to remove code smells

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const _ = require('lodash');
 const stopword = require('stopword');
 
-if(process.argv.length < 4){
+if(process.argv.length < 4) {
     console.log('Usage: node index.js *filename* *keyword*');
     process.exit(1);
 }
 
-const readFile = async function(fileName){
-    return await new Promise(function(resolve, reject){
+const readFile = async function(fileName) {
+    return await new Promise(function(resolve, reject) {
         fs.readFile(fileName, 'utf8', function(err, data) {
-            if(err){
+            if(err) {
                 reject(err);
             }
             resolve(data);
@@ -18,28 +18,25 @@ const readFile = async function(fileName){
     });
 };
 
-const splitUserStories = async function(uS){
-    return await new Promise(function(resolve){
-        if(uS.includes('\n\n\n')){
+const splitUserStories = async function(uS) {
+    return await new Promise(function(resolve) {
+        if(uS.includes('\n\n\n')) {
             resolve(uS.split('\n\n\n'));
-        }
-        else if(uS.includes('\r\n\r\n\r\n')){
+        } else if(uS.includes('\r\n\r\n\r\n')) {
             resolve(uS.split('\r\n\r\n\r\n'));
-        }
-        else{
+        } else{
             console.log('String was unable to be separated!');
             process.exit(1);
         }
-
     });
 };
 
-const selectImportantUserStories = async function(userStoriesSplit, keyword){
-    return await new Promise(function(resolve){
+const selectImportantUserStories = async function(userStoriesSplit, keyword) {
+    return await new Promise(function(resolve) {
         let importantUserStories = [];
-        _.each(userStoriesSplit, function(userStory){
+        _.each(userStoriesSplit, function(userStory) {
             const isImportant = userStory.includes(keyword);
-            if(isImportant){
+            if(isImportant) {
                 importantUserStories.push(userStory);
             }
         });
@@ -47,20 +44,18 @@ const selectImportantUserStories = async function(userStoriesSplit, keyword){
     });
 };
 
-const keyUserStories = async function(uSs){
-    return await new Promise(function(resolve){
+const keyUserStories = async function(uSs) {
+    return await new Promise(function(resolve) {
         let userStories = [];
-        _.each(uSs, function(uS){
-            const indexOfColon = uS.indexOf(":");
-            let key = "";
-            if(indexOfColon === 3){
+        _.each(uSs, function(uS) {
+            const indexOfColon = uS.indexOf(':');
+            let key = '';
+            if(indexOfColon === 3) {
                 key = uS.charAt(indexOfColon - 1);
-            }
-            else if(indexOfColon === 4){
+            } else if(indexOfColon === 4) {
                 key = uS.charAt(indexOfColon - 2) + uS.charAt(indexOfColon - 1);
             }
             let string = uS.substring(indexOfColon + 2);
-            
             userStories.push({
                 id: key,
                 string: string
@@ -70,34 +65,34 @@ const keyUserStories = async function(uSs){
     });
 };
 
-const formatUserStories = async function(uSs){
-    return await new Promise(function(resolve){
-        _.each(uSs, function(uS){
+const formatUserStories = async function(uSs) {
+    return await new Promise(function(resolve) {
+        _.each(uSs, function(uS) {
             uS.string = uS.string.replace(/:/g, '');
-            if(uS.string.includes('\n\n')){
+            if(uS.string.includes('\n\n')) {
                 uS.string = uS.string.replace(/\n/g, ' ');
-            }
-            else if(uS.string.includes('\r\n\r\n')){
+            } else if(uS.string.includes('\r\n\r\n')) {
                 uS.string = uS.string.replace(/\r\n/g, ' ');
             }
             uS.string = uS.string.replace('  ', ' ');
             uS.string = uS.string.toLowerCase();
             uS.string = uS.string.split(' ');
             uS.string = stopword.removeStopwords(uS.string);
-            uS.string = uS.string.filter(function(item, index, array){
-                return array.indexOf(item) == index;
+            uS.string = uS.string.filter(function(item, index, array) {
+                return array.indexOf(item) === index;
             });
         });
         resolve(uSs);
     });
 };
 
-const execute = async function(){
+const execute = async function() {
     const userStoriesText = await readFile(process.argv[2]);
     const userStoriesSplit = await splitUserStories(userStoriesText);
     const importantUserStories = await selectImportantUserStories(userStoriesSplit, process.argv[3]);
     const keyedUserStories = await keyUserStories(importantUserStories);
     const formattedUserStories = await formatUserStories(keyedUserStories);
+    console.log(formattedUserStories);
 };
 
 execute();
